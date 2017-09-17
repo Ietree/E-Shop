ecaff38b3c90225cabe94641aaae8af5637c3632
Root <Dylansuns@hotmail.com>
2017-09-17 22:16:53 +0800

    提交页面信息

--- eshop-parent/eshop-console/src/main/java/com/ietree/eshop/core/controller/CenterController.java
+++ eshop-parent/eshop-console/src/main/java/com/ietree/eshop/core/controller/CenterController.java
@@ -1,14 +1,8 @@
 package com.ietree.eshop.core.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.ietree.eshop.core.bean.TestTb;
-import com.ietree.eshop.core.service.TestTbService;
 
 /**
  * 后台管理
@@ -16,22 +10,50 @@ import com.ietree.eshop.core.service.TestTbService;
  * @author Root
  */
 @Controller
+@RequestMapping(value = "/control")
 public class CenterController {
-
-	@Autowired
-	private TestTbService testTbService;
-
 	/**
 	 * 返回值类型： 1、 ModelAndView ：跳转视图 + 数据 一般不用 2、void ：异步时ajax请求 3、String ：跳转视图 +
 	 * Model
 	 */
-	@RequestMapping(value = "/test/index.do")
+	@RequestMapping(value = "/index")
 	public String index(Model model) {
-		List<TestTb> list = testTbService.getAllBrands();
-		for (TestTb testTb : list) {
-			System.out.println(testTb.toString());
-		}
 		return "index";
+	}
+
+	@RequestMapping(value = "/top")
+	public String top(Model model) {
+		return "top";
+	}
+
+	@RequestMapping(value = "/main")
+	public String main(Model model) {
+		return "main";
+	}
+
+	@RequestMapping(value = "/left")
+	public String left(Model model) {
+		return "left";
+	}
+
+	@RequestMapping(value = "/right")
+	public String right(Model model) {
+		return "right";
+	}
+
+	@RequestMapping(value = "/frame/product_main")
+	public String productMain(Model model) {
+		return "frame/product_main";
+	}
+
+	@RequestMapping(value = "/frame/order_main")
+	public String orderMain(Model model) {
+		return "frame/order_main";
+	}
+
+	@RequestMapping(value = "/frame/product_left")
+	public String productLeft(Model model) {
+		return "frame/product_left";
 	}
 
 }
